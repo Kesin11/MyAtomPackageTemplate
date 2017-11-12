00f8c766aa1cccf49b61aea4b77d0eae03830ad8
Kenta Kase <kesin1202000@gmail.com>
2017-11-12 16:57:24 +0900

    Fix view spec

--- spec/my-atom-package-template-view-spec.js
+++ spec/my-atom-package-template-view-spec.js
@@ -3,7 +3,9 @@
 import MyAtomPackageTemplateView from '../lib/my-atom-package-template-view';
 
 describe('MyAtomPackageTemplateView', () => {
-  it('has one valid test', () => {
-    expect('life').toBe('easy');
+  it('has own className in root element', () => {
+    const view = new MyAtomPackageTemplateView();
+    const element = view.getElement()
+    expect(element.className).toBe('my-atom-package-template')
   });
 });
