580542384cfbb653b5176267cc793b82e51ee604
Kenta Kase <kesin1202000@gmail.com>
2017-11-12 12:53:45 +0900

    Generate package

---
+++ spec/my-atom-package-template-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import MyAtomPackageTemplateView from '../lib/my-atom-package-template-view';
+
+describe('MyAtomPackageTemplateView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
